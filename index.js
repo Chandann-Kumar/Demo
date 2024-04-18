833aefa010997c77367e15dfbfed9da13ccddea8
Chandan Kumar <kumarchandan1100k@gmail.com>
2024-04-18 14:26:58 +0530

    raj

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-const name = 'chandan kumar'
+const userName = 'chandan kumar'
+const userId = 1041;
+const state = 'Bihar'
+const city = 'Motihari'
 
-
+function getdetails(){
+    console.log(userName);
+    console.log(userId);
+    console.log(state);
+    console.log(city);
+}
